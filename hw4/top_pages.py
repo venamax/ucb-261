c252800458a0ee150cfadde498f8c779e33c93c3
Alejandro Rojas <ale@trixandtrax.com>
2016-02-05 18:42:45 -0300

    4.4 done

--- hw4/top_pages.py
+++ hw4/top_pages.py
@@ -2,22 +2,19 @@ from mrjob.job import MRJob
 import csv
 
 def csv_readline(line):
-    """Given a sting CSV line, return a list of strings."""
     for row in csv.reader([line]):
         return row
 
 class TopPages(MRJob):
 
     def mapper(self, line_no, line):
-        """Extracts the Vroot that was visited"""
         record = csv_readline(line)
-        yield record[1], 1
+        page_visitor = ((record[1],record[4]))
+        yield page_visitor, 1
                   
-    def reducer(self, page, visit_counts):
-        """Sumarizes the visit counts by adding them together.  If total visits
-        is more than 400, yield the results"""
+    def reducer(self, page_visitor, visit_counts):
         total = sum(i for i in visit_counts)
-        yield page, total
+        yield page_visitor, total
                   
         
 if __name__ == '__main__':
